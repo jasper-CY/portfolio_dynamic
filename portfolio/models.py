181c1f23ee90b5958d7c5c950fe0f16c20499342
jasper_yeeee <yeehaohao0108@gmail.com>
2022-12-25 15:37:07 +0800

    add sign/login/logout in views.py and html

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phonenumber = models.CharField(max_length=15)
+    description = models.TextField()
