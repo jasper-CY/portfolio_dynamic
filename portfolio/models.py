64f81194b8cb88224279bb142acb1921e6fbb057
jasper_yeeee <yeehaohao0108@gmail.com>
2022-12-25 17:38:44 +0800

    add blog/about, new blog database

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,16 @@ class Contact(models.Model):
     email = models.EmailField()
     phonenumber = models.CharField(max_length=15)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    authname = models.CharField(max_length=15)
+    img = models.ImageField(upload_to='blog',blank=True,null=True)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
