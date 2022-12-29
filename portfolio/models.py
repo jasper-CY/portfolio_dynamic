f024014689834dbfdbaa929cf96ad2684673f0ad
jasper_yeeee <yeehaohao0108@gmail.com>
2022-12-29 16:47:47 +0800

    add profile img and adjust blog.htmll

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,10 +12,12 @@ class Contact(models.Model):
 
 class Blogs(models.Model):
     title = models.CharField(max_length=60)
+    catego = models.CharField(max_length=10, null=False)
     description = models.TextField()
     authname = models.CharField(max_length=15)
     img = models.ImageField(upload_to='blog',blank=True,null=True)
     timeStamp = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
