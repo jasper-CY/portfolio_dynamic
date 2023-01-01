b0bf5acab982184d4e3734a2daa597e656d3f48c
jasper_yeeee <yeehaohao0108@gmail.com>
2023-01-01 20:42:00 +0800

    update settings.py

--- portfolio_dynamic/settings.py
+++ portfolio_dynamic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-817cwky$(zxckgm2@&#a_bg)b7uj@t26p)t0iaoy2jv^=e7#5y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['portfoliodynamic-production.up.railway.app']
+ALLOWED_HOSTS = ['*','portfoliodynamic-production.up.railway.app']
 
 
 # Application definition
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static'),
 ]
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
