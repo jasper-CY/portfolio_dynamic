4e05947a2737831b72f73bcf5db9faf9843a0fef
jasper_yeeee <yeehaohao0108@gmail.com>
2023-01-01 21:40:45 +0800

    update settings.py

--- portfolio_dynamic/settings.py
+++ portfolio_dynamic/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
