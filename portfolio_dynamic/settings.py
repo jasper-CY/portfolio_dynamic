dfc57921a6755d75f76089230a46250ec661130d
jasper_yeeee <yeehaohao0108@gmail.com>
2023-01-01 22:05:33 +0800

    update settings.py

--- portfolio_dynamic/settings.py
+++ portfolio_dynamic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-817cwky$(zxckgm2@&#a_bg)b7uj@t26p)t0iaoy2jv^=e7#5y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','portfoliodynamic-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,7 +129,7 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static'),
 ]
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
