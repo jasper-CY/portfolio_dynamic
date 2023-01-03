a237650381a825f173c32acca692f89dad4956e5
jasper_yeeee <yeehaohao0108@gmail.com>
2023-01-03 21:21:16 +0800

    update settings.py

--- portfolio_dynamic/settings.py
+++ portfolio_dynamic/settings.py
@@ -130,8 +130,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media/')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staticfiles/media/'
 
 
 # Default primary key field type
